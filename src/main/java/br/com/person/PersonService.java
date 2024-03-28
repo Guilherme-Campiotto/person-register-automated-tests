366a1659794b1eef8d6602ddbba0001be47090bf
Guilherme Campiotto <guilherme_campiotto@hotmail.com>
2024-03-28 09:03:00 -0300

    first commit

---
+++ src/main/java/br/com/person/PersonService.java
@@ -0,0 +1,5 @@
+package br.com.person;
+
+public class PersonService {
+
+}
---
+++ src/test/java/br/com/person/PersonServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.person;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+	@Test
+    @DisplayName("When creating a person with success should return should return Person object")
+	void testCreatePersonWhenSuccessShouldReturnPerson() {
+
+	}
+}
